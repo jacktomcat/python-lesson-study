26132bbf3e01025414a3326184a9ebe488ee6239
zhuhh <jackjboss@gmail.com>
2017-02-10 18:47:37 +0800

    新增数据库信息

--- hello.py
+++ hello.py
@@ -8,6 +8,9 @@ from user import User
 from modules.employee import Employee
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] ='jackjboss'
+app.config['SQLALCHEMY_DATABASE_URI']='mysql://upenv:upenv@localhost:3306/accelarator' #这里登陆的是root用户，要填上自己的密码，MySQL的默认端口是3306，填上之前创建的数据库名text1
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN']=True #设置这一项是每次请求结束后都会自动提交数据库中的变动
 
 @app.route('/')
 def index():
@@ -53,8 +56,22 @@ def get_current_user():
     """return jsonify(username='zhuhh',
                    email='jackjboss@163.com',
                    id=1)"""
-    return jsonify(tasks)  
+    return jsonify(tasks)
     #return json.dumps(tasks)          
+@app.route('/roles/getRoles')
+def getRoles(post_id):
+  Role.query.all()
+  return 'get roles'
+
+
+class Role(db.Model):
+     __tablename__ = 'roles'
+     id = db.Column(db.Integer,primary_key=True)
+     name = db.Column(db.String(64),unique=True)
+     #user = db.relationship('User',backref='role',lazy='dynamic')#建立两表之间的关系，其中backref是定义反向关系，lazy是禁止自动执行查询（什么鬼？）
+
+     def __repr__(self):
+      eturn '<Role {}> '.format(self.name)
 
 
 if __name__ == '__main__':
